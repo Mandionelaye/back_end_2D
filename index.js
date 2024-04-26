46562256169a723acfbf292ccf8ad0bd2a7add13
Mandionelaye <mandionelay99@gmail.com>
2024-04-26 16:53:12 +0000

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db  = require('./BasesDeDonnees/BD');
+const cors = require('cors');
 const routes = require('./Routes/Route');
 require('dotenv').config();
 
@@ -7,7 +8,10 @@ db.connection();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors());
+
 app.use(routes);
 
 app.listen(8000, ()=>(
